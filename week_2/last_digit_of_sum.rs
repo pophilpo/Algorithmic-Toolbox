61fe0a03220fd6ace807c91ea63c221b63acbf67
Philipp <philolippo@gmail.com>
2019-06-26 03:57:20 +0300

    Create last_digit_of_sum.rs

---
+++ week_2/last_digit_of_sum.rs
@@ -0,0 +1,24 @@
+fn main() {
+    let mut input = String::new();
+    std::io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+    let mut id = input.trim().parse::<u64>().expect("This is not a number!");
+
+    if id <= 1 {
+        println!("{}", id);
+    } else {
+        id = id + 2;
+
+        let mut prev = 1;
+        let mut current = 1;
+
+        for _ in 2..id {
+            let buffer = current;
+            current = (prev + current) % 10;
+            prev = buffer;
+        }
+
+        println!("{}", current - 1);
+    }
+}
